ad4ed2350460350789481b266c071cffbc8455c3
joaquinolivera <joaquin@Joaquin.localdomain>
2019-09-22 23:00:03 -0400

    add settgins layout

--- app/src/main/java/smarttraffic/smartparking/activities/SettingsActivity.java
+++ app/src/main/java/smarttraffic/smartparking/activities/SettingsActivity.java
@@ -3,6 +3,12 @@ package smarttraffic.smartparking.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import smarttraffic.smartparking.R;
 
 /**
  * Created by Joaquin on 09/2019.
@@ -12,9 +18,14 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    @BindView(R.id.saveSettings)
+    Button saveSettings;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_layout);
+        ButterKnife.bind(this);
     }
 
     @Override
